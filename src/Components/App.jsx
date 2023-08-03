7b12de3c2287d1a7451ea9259acc0fbbfb90dbc2
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-08-03 12:28:34 +0300

    aug project 1

---
+++ src/Components/App.jsx
@@ -0,0 +1,11 @@
+import "../Style/style.css";
+
+function App() {
+    return (
+        <div className="check">
+            <p>Yonatan Afewerk Teshome</p>
+        </div>
+    );
+}
+
+export default App;
