e8a4d0401e21bbc5c61dc825a037eaaf194fd490
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-08-15 22:51:30 +0300

    typo fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <section>
                 <h1>A Simple Bookmark Manager</h1>
                 <p>
-                    A clean nad simple interface to organize your favorite
+                    A clean and simple interface to organize your favorite
                     websites. Open a new browser tab and see your sites load
                     instantly. Try it for free.
                 </p>
